92b8ce3d12185f6c254db86bcd31b9f19442d7d8
Saenro Gonsalves <aenros09602@gmail.com>
2024-01-11 10:35:49 +0530

    add comit

---
+++ Github1/src/DemoB4/add2.java
@@ -0,0 +1,21 @@
+package DemoB4;
+
+import java.util.Scanner;
+
+public class add2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x,y,sum;
+
+		System.out.println("enter two numbers");
+		Scanner scan = new Scanner(System.in);
+		x=scan.nextInt();
+		y=scan.nextInt();
+		sum=x+y;
+		System.out.println("the sum is: "+sum);
+		
+	}
+
+}
