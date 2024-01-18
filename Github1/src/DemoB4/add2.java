c91e3f2eca19bcf3b9d47cda8357836adcaae964
Saenro Gonsalves <aenros09602@gmail.com>
2024-01-18 09:30:04 +0530

    asdwegh moye

--- Github1/src/DemoB4/add2.java
+++ Github1/src/DemoB4/add2.java
@@ -7,14 +7,14 @@ public class add2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int x,y,sum,w;
+		int x,y,sum,w,moye;
 
 		System.out.println("enter two numbers");
 		Scanner scan = new Scanner(System.in);
 		x=scan.nextInt();
 		y=scan.nextInt();
 		sum=x+y;
-		System.out.println("the sum is: "+sum);
+		System.out.println("the sum is moye moye: "+sum);
 		
 	}
 
